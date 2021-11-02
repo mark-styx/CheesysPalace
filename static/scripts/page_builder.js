ed6061bbc643a1323362a3358962642fcfa6a761
Mark Styx <mark.styx@hotmail.com>
2021-11-02 16:55:50 -0400

    Update page_builder.js

--- static/scripts/page_builder.js
+++ static/scripts/page_builder.js
@@ -3,7 +3,7 @@ request = function(url) {
     return fetch(url,options={'method':'post'})
 }
 
-fetch( 'http://10.0.0.47:5000/check_devices')
+fetch( 'http://10.0.0.177:5000/check_devices')
     .then( response => response.json() )
     .then( devices => {
         console.log(devices)
@@ -17,8 +17,8 @@ fetch( 'http://10.0.0.47:5000/check_devices')
                 let btnOff = document.createElement('button')
                 btnOn.innerHTML = 'Turn on ' + deviceAlias
                 btnOff.innerHTML = 'Turn off ' + deviceAlias
-                btnOn.value = 'http://10.0.0.47:5000/device_action/' + deviceAlias + '/power/1'
-                btnOff.value = 'http://10.0.0.47:5000/device_action/' + deviceAlias + '/power/0' 
+                btnOn.value = 'http://10.0.0.177:5000/device_action/' + deviceAlias + '/power/1'
+                btnOff.value = 'http://10.0.0.177:5000/device_action/' + deviceAlias + '/power/0' 
                 btnOn.addEventListener('click',function() {request(btnOn.value)})
                 btnOff.addEventListener('click',function() {request(btnOff.value)})
                 parent.appendChild(btnOn)
